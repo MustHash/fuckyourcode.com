c342b9fe9f70ef251d584ceb8922605512425b2e
Couto <lcouto87@gmail.com>
2013-03-29 16:36:46 +0000

    add hash

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,12 @@
 
         setRoute = function (index) {
             var stateObj = { 'index': index };
-            history.pushState(stateObj, '', index);
+
+            if (!history && typeof history.pushState === 'function') {
+                history.pushState(stateObj, '', index);
+            } else {
+                window.location.href = location.href.replace(/(#\!\/)*\d+/, '#!/' + index)
+            }
         },
 
         loadQuotes = function (url, cb) {
