c0fa454cfabac5f52b40016f0ad06a6dc00f5333
Couto <lcouto87@gmail.com>
2013-04-05 00:21:35 +0100

    Fix pushState. Refactor code.
    * Fix pushState/hashBang (in older IE)
    * Refactor code to use objects to group functions
    * Now with back/forward buttons in recent browsers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,151 +1,233 @@
 (function () {
 
-    var $body = document.querySelector('body'),
-        $placeholder = document.querySelector('.placeholder'),
-        $refresh = document.querySelector('.refresh'),
-        $$placeholder = moofx($placeholder),
-        quotes = [],
+    'use strict';
 
-        // stores previous quotes
-        seen_quotes = [],
-
-        // stores previous loaded theme
-        prev_theme_index = null,
-
-        // array of theme names, to apply a 'theme-*' class to body
-        themes = ['light', 'yellow', 'green', 'blue', 'magenta'],
-
-        getRoute = function () {
-            var matches = window.location.href.match(/[0-9]+/),
-                index = (matches && matches.length) ? matches[matches.length - 1] : 0;
-
-            return parseInt(index, 10);
-        },
-
-        setRoute = function (index) {
-            var stateObj = { 'index': index };
-
-            if (history && typeof history.pushState === 'function') {
-                history.pushState(stateObj, '', index);
-            } else {
-                window.location.hash = location.hash.replace(/(\!\/)*\d+/, '!/' + index);
-            }
-        },
-
-        loadQuotes = function (url, cb) {
-            console.log('Fuck your console, too');
-
-            var request = new XMLHttpRequest();
-            request.onload = function () {
-
-                if (this.status === 200 && this.readyState === 4) {
-                    return cb(null, this.responseText);
+    /**
+     * [utils description]
+     * @type {Object}
+     */
+    var utils = {
+        addEvent : (function () {
+            if (window.addEventListener) {
+                return function (el, evt, fn) {
+                    return el.addEventListener(evt, fn, false);
+                };
+            } else if (window.attachEvent) {
+                return function (el, evt, fn) {
+                    evt = (evt.slice(0, 1).toUpperCase() + evt.substr(1));
+                    return el.attachEvent('on' + evt, function () {
+                        fn.apply(el, window.Event)
+                    });
                 }
+            } else {
+                return function (el, evt, fn) {
+                    evt = (evt.slice(0, 1).toUpperCase() + evt.substr(1));
+                    el[evt] = fn;
+                    return el;
+                }
+            }
+        }()),
+        proxy : function (fn, context) {
+            var args = [].slice.call(arguments, 2)
+            return function () {
+                fn.apply(context, args.concat([].slice.call(arguments, 0)))
+            }
+        }
+    },
+    /**
+     * [quotes description]
+     * @type {Object}
+     */
+    quotes = {
+        url: 'quotes.json',
+        $placeholder: document.querySelector('.placeholder'),
+        $$placeholder: moofx(document.querySelector('.placeholder')),
+        list: [],
+        seen: [],
+        current: null,
+        refresh: function (index, cb) {
+            var marginTop = 0 - this.$placeholder.offsetHeight / 2 + 20;
 
-                return cb(err);
-            };
-            request.open("GET", url, true);
+            this.$$placeholder.animate({
+                'opacity': 0,
+                'margin-top': marginTop + 'px'
+            }, {
+                duration : '0.5s',
+                equation: 'ease-in-out',
+                callback: utils.proxy(function () {
+                    this.unset();
+                    this.set(index);
+                    if (cb) { cb(); }
+                }, this)
+            });
+
+
+        },
+        load: function (cb) {
+            var request = new XMLHttpRequest();
+
+            console.log('Fuck your console, too');
+            request.onload = utils.proxy(function (req) {
+                if (req.status === 200 && req.readyState === 4) {
+                    this.list = JSON.parse(req.responseText);
+                    return cb(null, this.list);
+                }
+                return cb(req);
+            }, this, request);
+            request.open("GET", this.url, true);
             request.send();
         },
+        set: function (idx) {
+            var index       = (this.list[idx]) ? idx : this.random(),
+                fuckRegex   = /(^|\s)(fuck)/ig,
+                quote       = this.list[index],
+                marginTop   = 0,
+                marginTopBefore = 0;
 
-        seenQuote = function( index ) {
-            var array_len = seen_quotes.length,
-                max = 7;
-
-            if ( array_len === max )
-                seen_quotes.splice(0, 1);
-
-            seen_quotes.push( index );
-
-        },
-
-        // calculate the index of the theme randomnly
-        generateTheme = function() {
-            var themes_length = themes.length;
-
-            return Math.floor(Math.random() * (themes_length));
-        },
-
-        // generate quote index
-        generateQuote = function() {
-            var index;
-
-            do {
-               index = Math.floor(Math.random()*quotes.length);
-            } while( seen_quotes.indexOf(index) != -1 );
-
-            return index;
-        },
-
-        setQuote = function (idx) {
-            var index = (idx && quotes[idx]) ? idx : generateQuote(),
-                quote = quotes[index].replace(/(^|\s)(fuck)/ig, '$1<span>$2</span>'),
-
-                theme_index = 0,
-                marginTop,
-                marginTopBefore;
-
-            // add to seen quote
-            seenQuote(index);
-            setRoute(index);
-
-            // dont ever repeat the same theme
-            do {
-                theme_index = generateTheme();
-            } while (theme_index === prev_theme_index);
-
-            prev_theme_index = theme_index;
-
-            // add class to body
-            $body.className = 'theme-'+themes[theme_index];
+            this.current = index;
 
             // print quote into the dom element
-            $placeholder.innerHTML = quote;
+            this.$placeholder.innerHTML = quote.replace(fuckRegex, '$1<span>$2</span>');
 
-            marginTop = - $placeholder.offsetHeight / 2;
+            marginTop = - this.$placeholder.offsetHeight / 2;
             marginTopBefore = marginTop - 20;
 
-            $$placeholder.style({'opacity':0, 'margin-top': marginTopBefore + 'px'});
-            $$placeholder.animate({
+            this.$$placeholder.style({
+                'opacity': 0,
+                'margin-top': marginTopBefore + 'px'
+            }).animate({
                 'opacity': 1,
                 'margin-top': marginTop + 'px'
             }, {
                 duration : '0.5s',
                 equation: 'ease-in-out'
             });
-        },
 
-        unsetQuote = function () {
-            var children = [].slice.call($placeholder.childNodes, 0),
+            return this;
+        },
+        unset : function () {
+            var children = [].slice.call(this.$placeholder.childNodes, 0),
                 i = children.length - 1;
 
             for (i; i >= 0; i -= 1) {
-                children[i].parentNode.removeChild(children[i]);
+                this.$placeholder.removeChild(children[i]);
             }
-        };
 
-    loadQuotes('quotes.json', function (err, data) {
-        quotes = JSON.parse(data);
-        route = getRoute();
-        setQuote(route);
+            this.current = null;
+
+            return this;
+        },
+        mark: function (idx) {
+            var len = this.seen.length,
+                max = 7;
+
+            if (len === max) { this.seen.shift(); }
+            this.seen.push(idx);
+
+            return this;
+        },
+        random: function () {
+            var index = 0;
+
+            do { index = Math.floor(Math.random() * this.list.length); }
+            while (this.seen.indexOf(index) !== -1);
+
+            return index;
+        }
+    },
+    /**
+     * [router description]
+     * @type {Object}
+     */
+    router = {
+        on: function (fn) {
+            window.onpopstate = fn;
+        },
+        set: function (index) {
+            var stateObj = { 'index': index };
+
+            if (history && typeof history.pushState === 'function') {
+                history.pushState(stateObj, '', index);
+            } else {
+                location.hash = ((/[0-9]+/).test(location.hash)) ?
+                        location.hash.replace(/\!\/*\d+/, '!/' + index) :
+                        '!/' + index;
+            }
+
+            return this;
+        },
+        replace: function (index) {
+            var stateObj = { 'index': index };
+            if (history && typeof history.replaceState === 'function') {
+                history.replaceState(stateObj, '', index);
+            } else {
+                location.hash = ((/[0-9]+/).test(location.hash)) ?
+                        location.hash.replace(/\!\/*\d+/, '!/' + index) :
+                        '!/' + index;
+            }
+        },
+        get: function () {
+            var matches = window.location.href.match(/[0-9]+/),
+                index   = ((matches && matches.length) || matches === 0) ?
+                        matches[matches.length - 1] :
+                        quotes.random();
+
+            return parseInt(index, 10);
+        }
+    },
+    /**
+     * [themes description]
+     * @type {Object}
+     */
+    themes = {
+        list: ['light', 'yellow', 'green', 'blue', 'magenta'],
+        prevIndex: null,
+        random: function () {
+            return Math.floor(Math.random() * (this.list.length));
+        },
+        set: function (el) {
+            var index = 0;
+
+            el = el || document.body;
+
+            // dont ever repeat the same theme
+            do { index = this.random(); }
+            while (index === this.prevIndex);
+
+            this.prevIndex = index;
+
+            el.className = 'theme-' + this.list[index];
+
+            return this;
+        }
+    },
+
+    $refresh = document.querySelector('.refresh')
+
+    utils.addEvent($refresh, 'click', function (evt) {
+        evt.preventDefault();
+        quotes.refresh(null, function () {
+            themes.set();
+            quotes.mark(quotes.current);
+            router.set(quotes.current)
+        });
     });
 
-    $refresh.addEventListener('click', function (evt) {
-        var marginTop = - $placeholder.offsetHeight / 2 + 20;
+    quotes.load(function () {
+        var index = router.get();
+        quotes.set(index);
+        quotes.mark(index);
+        themes.set();
+        router.replace(quotes.current)
 
-        evt.preventDefault();
-
-        $$placeholder.animate({
-            'opacity': 0,
-            'margin-top': marginTop + 'px'
-        }, {
-            duration : '0.5s',
-            equation: 'ease-in-out',
-            callback: function () {
-                unsetQuote();
-                setQuote();
-            }
+        router.on(function () {
+            index = history.state.index;
+            quotes.unset();
+            quotes.refresh(index);
+            quotes.mark(index);
+            themes.set();
         });
+
     });
 
 }());
